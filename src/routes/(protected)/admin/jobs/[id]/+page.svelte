7c0305e67bda51ab4d98b49a23696479164a2cca
Jumia <dhubdevs@gmail.com>
2025-05-09 22:54:23 +0100

    push

---
+++ src/routes/(protected)/admin/jobs/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import JobForm from '$lib/components/forms/JobForm.svelte';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
+{#await data.getJob}
+	<p>Loading...</p>
+{:then result}
+	{@const job = result.data}
+	<JobForm {job} />
+{/await}
