fa228d25d5664c5561b4f26590b1c1f0144f74eb
Jumia <dhubdevs@gmail.com>
2025-05-09 22:56:17 +0100

    push

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Binoculars from '$lib/components/icons/Binoculars.svelte';
+	import { Button } from '$lib/components/ui/button';
+</script> 
+ 
+<div class="flex items-center justify-center h-screen w-full">
+	<div class="flex flex-col items-center gap-2">
+		<Binoculars />
+		<h1 class="text-2xl font-semibold">{page.status}: {page.error?.message}</h1>
+		<Button href="/">Homepage 👉</Button>
+	</div>
+</div>
