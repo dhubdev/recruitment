65d2a6faa232ae70c6ede3279b3abd874908d5a7
Jumia <dhubdevs@gmail.com>
2025-05-09 16:46:54 +0100

    Update +page.svelte

--- src/routes/(protected)/admin/+page.svelte
+++ src/routes/(protected)/admin/+page.svelte
@@ -7,11 +7,11 @@
 <Admin>
 	<div class="flex h-full w-full flex-col items-center justify-center gap-4">
 		<div class="flex flex-col text-center leading-[1]">
-			<h2 class="text-2xl font-semibold">Ready to Create Your AI Blog?</h2>
+			<h2 class="text-2xl font-semibold">Ready to Create Your First Job?</h2>
 			<p class="text-muted-foreground">Click the button below to create one</p>
 		</div>
-		<Button href="/admin/blogs/create">
-			Create Blog
+		<Button href="/admin/jobs/new">
+			Create Job
 			<PlusIcon class="size-4" />
 		</Button>
 	</div>
