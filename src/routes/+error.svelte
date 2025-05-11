f919e61d998d3de13f97192d2468a6abcbdffb70
Jumia <dhubdevs@gmail.com>
2025-05-11 16:31:31 +0100

    push

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import Binoculars from '$lib/components/icons/Binoculars.svelte';
+	import BinocularsIcon from '$lib/components/icons/binoculars-icon.svelte';
 	import { Button } from '$lib/components/ui/button';
 </script> 
  
 <div class="flex items-center justify-center h-screen w-full">
 	<div class="flex flex-col items-center gap-2">
-		<Binoculars />
+		<BinocularsIcon />
 		<h1 class="text-2xl font-semibold">{page.status}: {page.error?.message}</h1>
 		<Button href="/">Homepage 👉</Button>
 	</div>
