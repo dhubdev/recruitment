3d8862d6b8f7a0f90579fce8019eebb708cefbbc
Jumia <dhubdevs@gmail.com>
2025-05-12 11:27:17 +0100

    Update +page.svelte

--- src/routes/(protected)/admin/jobs/+page.svelte
+++ src/routes/(protected)/admin/jobs/+page.svelte
@@ -30,9 +30,9 @@
 		title: job.title,
 		location: job.location,
 		nature: job.nature,
-		closingDate: toDatetimeLocal(job.closingDate),
-		createdAt: toDatetimeLocal(job.xata_createdat.toLocaleString()),
-		updatedAt: toDatetimeLocal(job.xata_updatedat.toLocaleString()),
+		closingDate: job.closingDate ? job.closingDate : '',
+		createdAt: job.xata_createdat.toLocaleString(),
+		updatedAt: job.xata_updatedat.toLocaleString(),
 		file: job.file ? (job.file as iFile)?.url : ''
 	})
 </script>
