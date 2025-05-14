1647538f952a8b631a973fd9f08754a9d0bde569
Jumia <dhubdevs@gmail.com>
2025-05-14 14:09:50 +0100

    Update Brand.svelte

--- src/lib/components/sections/Brand.svelte
+++ src/lib/components/sections/Brand.svelte
@@ -8,13 +8,13 @@
 	let { class: className, ...rest }: Props = $props()
 </script>
 
-<a href="/" {...rest}>
+<a href="/" {...rest} class={className}>
 	<div
 		class="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary dark:bg-white"
 	>
 		<Logo class="size-6" />
 	</div>
-	<div class="-mt-3 grid flex-1 text-left text-sm leading-[.2]">
+	<div class="grid flex-1 text-left text-sm leading-[.2]">
 		<p class="text-lg font-semibold text-primary dark:text-white">Jordan</p>
 		<span class="text-shade-3 dark:text-muted-foreground">Recruitments</span>
 	</div>
