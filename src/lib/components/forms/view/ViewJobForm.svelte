92afbaa828319acfec6deaf9ef3be9e865213b16
Jumia <dhubdevs@gmail.com>
2025-05-27 11:17:19 +0100

    Update ViewJobForm.svelte

--- src/lib/components/forms/view/ViewJobForm.svelte
+++ src/lib/components/forms/view/ViewJobForm.svelte
@@ -10,17 +10,23 @@
 	const file = job.file as iFile;
 </script>
 
-<div class="flex flex-col gap-2">
-	<div class="grid grid-cols-[40px_1fr] gap-4">
-		<img src={file.url} alt={job.source} />
-		<span class="text-sm font-medium">{job.source}</span>
-	</div>
+<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-1">
+		<div class="grid grid-cols-[40px_1fr] gap-4">
+			<img src={file.url} alt={job.source} />
+			<span class="text-sm font-medium">{job.source}</span>
+		</div>
 
-	<div class="">
-		{job.nature}
+		<div>
+			Location: {job.nature}
+		</div>
+		<div>
+			Nature: {job.nature}
+		</div>
 	</div>
 
 	<div class="prose dark:prose-invert">
+		<h2>About the Job</h2>
 		{@html job.content}
 	</div>
 </div>
