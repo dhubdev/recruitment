56caca014c1f60073dac11c56e231ba8b6c80e3a
Jumia <dhubdevs@gmail.com>
2025-05-27 11:14:37 +0100

    Update ViewJobForm.svelte

--- src/lib/components/forms/view/ViewJobForm.svelte
+++ src/lib/components/forms/view/ViewJobForm.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-	import type { iJob } from "$lib/interface";
+	import type { iFile, iJob } from '$lib/interface';
 
-  interface Props {
-    class?: string;
-    job: iJob
-  }
+	interface Props {
+		class?: string;
+		job: iJob;
+	}
 
-  let { class: className, job }: Props = $props()
+	let { class: className, job }: Props = $props();
+	const file = job.file as iFile;
 </script>
 
+<div class="flex flex-col gap-2">
+	<div class="grid grid-cols-[40px_1fr] gap-4">
+		<img src={file.url} alt={job.source} />
+		<span class="text-sm font-medium">{job.source}</span>
+	</div>
 
-<div class="prose dark:prose-invert">
-  {@html job.content}
+	<div class="">
+		{job.nature}
+	</div>
+
+	<div class="prose dark:prose-invert">
+		{@html job.content}
+	</div>
 </div>
