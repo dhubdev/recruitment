ec7547b7a29b30f41299c0ff577cbdf5a34bc5eb
Jumia <dhubdevs@gmail.com>
2025-06-05 12:45:56 +0100

    Update +page.server.ts

--- src/routes/(app)/applications/[id]/+page.server.ts
+++ src/routes/(app)/applications/[id]/+page.server.ts
@@ -1,9 +1,15 @@
 import { getApplication } from '$lib/xata/application';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, url, locals }) => {
 
   const { id } = params
+  const {userId} = locals.auth()
+  const pathname = url.pathname
+  if (!userId) {
+    throw redirect(302, `/sign-in?redirect=${pathname}`)
+  }
 
   const result = await getApplication(id)
   return { result };
