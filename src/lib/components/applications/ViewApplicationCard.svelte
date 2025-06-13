b0fc8366fe9f96628f28606f1c92e87ec070c2fb
Jumia <dhubdevs@gmail.com>
2025-06-13 12:18:59 +0100

    Update ViewApplicationCard.svelte

--- src/lib/components/applications/ViewApplicationCard.svelte
+++ src/lib/components/applications/ViewApplicationCard.svelte
@@ -13,7 +13,8 @@
   const applicationLetter = al as iFile
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-2 gap-2">
-  <iframe class="aspect-video w-full" src={coverLetter.url} frameborder="0" title="Cover Letter"></iframe>
-  <iframe class="aspect-video w-full" src={applicationLetter.url} frameborder="0" title="Resume"></iframe>
+<div class="grid grid-cols-1 gap-4">
+  <iframe class="size-full" src={coverLetter.url} frameborder="0" title="Cover Letter"></iframe>
+  <hr/>
+  <iframe class="size-full" src={applicationLetter.url} frameborder="0" title="Resume"></iframe>
 </div>
