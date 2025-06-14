dbcfe41ff0c931654ffab351733f73468dc7efae
Jumia <dhubdevs@gmail.com>
2025-06-14 20:43:41 +0100

    Update +page.server.ts

--- src/routes/(protected)/admin/jobs/[id]/+page.server.ts
+++ src/routes/(protected)/admin/jobs/[id]/+page.server.ts
@@ -1,13 +1,15 @@
 import { getJob } from '$lib/xata/job';
 import type { iResult } from '@toolsntuts/utils';
 import type { PageServerLoad } from './$types';
+import type { iDocumentCategory } from '$lib/interface';
 
 export const load = (async ({ params, parent }) => {
 
   const { getDocumentCategories } = await parent()
   const { id } = params
 
-  const documentCategories = await getDocumentCategories as iResult
+  const result = await getDocumentCategories as iResult
 
+  const documentCategories = result.data as iDocumentCategory[]
   return { getJob: getJob(id), documentCategories };
 }) satisfies PageServerLoad;
