d8f8d9aa42ee836b37a83a90a6f767f0cf7fc9b5
Jumia <dhubdevs@gmail.com>
2025-06-14 10:55:45 +0100

    Update feature.svelte

--- src/lib/components/marketing/feature.svelte
+++ src/lib/components/marketing/feature.svelte
@@ -4,16 +4,14 @@
 </script>
 
 <!-- Card Variant : soft  -->
-<section
-	class="[--color-primary:theme(color.indigo.500)] [--color-secondary-foreground:theme(color.indigo.600)] [--color-secondary:theme(color.indigo.100)] dark:[--color-primary:theme(color.indigo.400)] dark:[--color-secondary-foreground:theme(color.indigo.500)] dark:[--color-secondary:theme(color.indigo.400)]"
->
-	<div class="bg-muted/50 py-24">
-		<div class="mx-auto w-full max-w-5xl px-6">
+<section>
+	<div class="py-24">
+		<div class="mx-auto w-full max-w-5xl">
 			<div>
 				<h2 class="text-4xl font-semibold text-foreground">
 					Your Partner in Care, Cleaning, and Security Jobs
 				</h2>
-				<p class="mb-12 mt-4 text-balance text-lg text-muted-foreground">
+				<p class="mb-12 mt-4 text-balance text-muted-foreground">
 					At Jordan recruitments, we understand the importance of finding the right talent for your
 					care, cleaning, and security needs. Whether you're an employer seeking dedicated
 					professionals or a job seeker looking for meaningful opportunities, we've got you covered.
@@ -23,26 +21,30 @@
 				</div>
 			</div>
 
-			<div class="flex flex-col items-center">
-				<h2 class="prose dark:prose-invert">For Employers</h2>
-				<p class="text-muted-foreground">Tailored solutions towards your business</p>
+			<div class="flex flex-col items-center mt-12">
+				<h2 class="text-xl md:text-2xl">For Employers</h2>
+				<p class="text-muted-foreground text-lg">Tailored solutions towards your business</p>
 			</div>
 			<div
 				class="relative mt-16 grid gap-12 border-b border-foreground/10 pb-12 [--radius:1rem] md:grid-cols-2"
 			>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Care Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Find compassionate and qualified professionals to enhance your caregiving team. From nurses to support staff, we connect you with individuals committed to providing exceptional care.
+					<h3 class="text-lg font-semibold text-foreground">Care Jobs</h3>
+					<p class="my-4 text-muted-foreground">
+						Find compassionate and qualified professionals to enhance your caregiving team. From
+						nurses to support staff, we connect you with individuals committed to providing
+						exceptional care.
 					</p>
 					<Card variant="soft" class="aspect-video overflow-hidden px-6">
 						<Card class="h-full translate-y-6" />
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Cleaning Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Ensure your spaces shine with our top-notch cleaning professionals. Whether it's commercial or residential, we match you with skilled individuals dedicated to maintaining a clean and healthy environment.
+					<h3 class="text-lg font-semibold text-foreground">Cleaning Jobs</h3>
+					<p class="my-4 text-muted-foreground">
+						Ensure your spaces shine with our top-notch cleaning professionals. Whether it's
+						commercial or residential, we match you with skilled individuals dedicated to
+						maintaining a clean and healthy environment.
 					</p>
 
 					<Card variant="soft" class="aspect-video overflow-hidden">
@@ -50,9 +52,11 @@
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Security Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Safety is paramount. Discover skilled security personnel to protect your premises. Our pool of candidates includes trained security officers and personnel to meet your specific security requirements.
+					<h3 class="text-lg font-semibold text-foreground">Security Jobs</h3>
+					<p class="my-4 text-muted-foreground">
+						Safety is paramount. Discover skilled security personnel to protect your premises. Our
+						pool of candidates includes trained security officers and personnel to meet your
+						specific security requirements.
 					</p>
 
 					<Card variant="soft" class="aspect-video overflow-hidden">
@@ -60,9 +64,11 @@
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Efficient Recruitment Process</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						We streamline the hiring process, making it easy for you to find the right candidates. Our platform allows you to post jobs, review applications, and connect with potential hires seamlessly.
+					<h3 class="text-lg font-semibold text-foreground">Efficient Recruitment Process</h3>
+					<p class="my-4 text-muted-foreground">
+						We streamline the hiring process, making it easy for you to find the right candidates.
+						Our platform allows you to post jobs, review applications, and connect with potential
+						hires seamlessly.
 					</p>
 
 					<Card variant="soft" class="aspect-video overflow-hidden">
@@ -71,26 +77,28 @@
 				</div>
 			</div>
 
-			<div class="flex flex-col items-center">
-				<h2 class="prose dark:prose-invert">For Job Seekers</h2>
-				<p class="text-muted-foreground">Explore rewarding opportunities</p>
+			<div class="flex flex-col items-center mt-12">
+				<h2 class="text-xl md:text-2xl">For Job Seekers</h2>
+				<p class="text-muted-foreground text-lg">Explore rewarding opportunities</p>
 			</div>
-      			<div
+			<div
 				class="relative mt-16 grid gap-12 border-b border-foreground/10 pb-12 [--radius:1rem] md:grid-cols-2"
 			>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Care Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Make a difference in people's lives. Explore opportunities in caregiving, nursing, and support roles.
+					<h3 class="text-lg font-semibold text-foreground">Care Jobs</h3>
+					<p class="my-4 text-muted-foreground">
+						Make a difference in people's lives. Explore opportunities in caregiving, nursing, and
+						support roles.
 					</p>
 					<Card variant="soft" class="aspect-video overflow-hidden px-6">
 						<Card class="h-full translate-y-6" />
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Cleaning Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Join our network of cleaning professionals dedicated to maintaining pristine environments.
+					<h3 class="text-lg font-semibold text-foreground">Cleaning Jobs</h3>
+					<p class="my-4 text-muted-foreground">
+						Join our network of cleaning professionals dedicated to maintaining pristine
+						environments.
 					</p>
 
 					<Card variant="soft" class="aspect-video overflow-hidden">
@@ -98,8 +106,8 @@
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Security Jobs</h3>
-					<p class="my-4 text-lg text-muted-foreground">
+					<h3 class="text-lg font-semibold text-foreground">Security Jobs</h3>
+					<p class="my-4 text-muted-foreground">
 						Protect and serve. Find fulfilling roles in the security industry.
 					</p>
 
@@ -108,9 +116,10 @@
 					</Card>
 				</div>
 				<div>
-					<h3 class="text-xl font-semibold text-foreground">Career and Resource Guidance</h3>
-					<p class="my-4 text-lg text-muted-foreground">
-						Access valuable resources, tips, and guidance to enhance your job search and career growth. We're here to support you every step of the way.
+					<h3 class="text-lg font-semibold text-foreground">Career and Resource Guidance</h3>
+					<p class="my-4 text-muted-foreground">
+						Access valuable resources, tips, and guidance to enhance your job search and career
+						growth. We're here to support you every step of the way.
 					</p>
 
 					<Card variant="soft" class="aspect-video overflow-hidden">
