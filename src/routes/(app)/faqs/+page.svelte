b027102e2d614ab5c615cc08f27150a2d94d251e
Jumia <dhubdevs@gmail.com>
2025-06-20 10:34:49 +0100

    Update +page.svelte

--- src/routes/(app)/faqs/+page.svelte
+++ src/routes/(app)/faqs/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import Wrap from '$lib/components/ui/wrap/wrap.svelte';
+import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 </script>
-<div class="prose dark:prose-invert">
+
+<Wrap class="prose dark:prose-invert">
 	<h1>Frequently Asked Questions</h1>
-<p>Coming Soon...</p>
-</div>
+	<p>Coming Soon...</p>
+</Wrap>
